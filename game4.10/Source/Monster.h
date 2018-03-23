4d898f35a4e692fe20dfc75dca45b78f1d18a0f3
asdzxc55888 <asdzxc55888@gmail.com>
2018-03-23 12:16:51 +0800

    npcObject OnMove 錯誤

---
+++ game4.10/Source/Monster.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "npcObject.h"
+
+namespace game_framework {
+	
+	class Monster :public npcObject
+	{
+	public:
+
+	private:
+		int name;
+	};
+
+}
--- game4.10/Source/mygame.h
+++ game4.10/Source/mygame.h
@@ -41,6 +41,8 @@
 #include "CEraser.h"
 #include "CBall.h"
 #include "CBouncingBall.h"
+#include "Monster.h"
+
 
 namespace game_framework {
 	/////////////////////////////////////////////////////////////////////////////
@@ -71,7 +73,7 @@ namespace game_framework {
 		void OnShow();									// 顯示這個狀態的遊戲畫面
 	private:
 		CMovingBitmap Background;                       // 背景
-		CAnimation *menuBtn[4];
+		CAnimation *menuBtn[4];							
 		bool isMouseOn[4];
 		bool isLoadingBitmaps;
 		bool isPlayAudio;
@@ -99,14 +101,8 @@ namespace game_framework {
 		void OnMove();									// 移動遊戲元素
 		void OnShow();									// 顯示這個狀態的遊戲畫面
 	private:
-		const int		NUMBALLS;	// 球的總數
-		CMovingBitmap	gameBackground;	// 背景圖
-		CMovingBitmap	help;		// 說明圖
-		CBall			*ball;		// 球的陣列
-		CMovingBitmap	corner;		// 角落圖
-		CEraser			eraser;		// 拍子
-		CInteger		hits_left;	// 剩下的撞擊數
-		CBouncingBall   bball;		// 反覆彈跳的球
+		CMovingBitmap Background;
+		Monster tentacle;
 	};
 
 	/////////////////////////////////////////////////////////////////////////////
